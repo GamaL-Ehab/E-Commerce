06b94678ff6ef4889ff02fac9de13bb1b5aaec7e
Gamal-Ehab <gamalehabg@gmail.com>
2025-01-29 17:02:10 +0200

    installing Packages & Creating all Components

---
+++ src/app/layout/additions/navbar/navbar.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { FlowbiteService } from '../../../shared/services/flowbite/flowbite.service';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+  constructor(private flowbiteService: FlowbiteService) {}
+
+  ngOnInit(): void {
+    this.flowbiteService.loadFlowbite(flowbite => {
+      // Your custom code here
+      console.log('Flowbite loaded', flowbite);
+    });
+  }
+}
